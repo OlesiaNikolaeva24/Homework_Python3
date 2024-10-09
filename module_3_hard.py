80197de0b4882ed5a882e4a72bd119e60cc2fd8e
OlesiaNikolaeva24 <kobaltbrom@gmail.com>
2024-10-09 15:45:37 +0900

    08/10 "Раз, два, три, четыре, пять .... Это не всё?"

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,29 +1,27 @@
-def calculate_structure_sum(*args):
-    global summa
-    if not len(args) > 1:
-        if isinstance(args, str):
-            summa += len(args)
-        elif isinstance(args, int):
-            summa += args
-        else:
-            continue
-    for i in args:
-        calculate_structure_sum(*args)
+def calculate_structure_sum(*args, total=0):
+    for arg in args:
+        if isinstance(arg, dict):
+            for d in list(arg.items()):
+                total += calculate_structure_sum(d)
+        elif isinstance(arg, list) or isinstance(arg, tuple) or isinstance(arg, set):
+            for element in arg:
+                total += calculate_structure_sum(element)
+        elif isinstance(arg, str):
+            total += len(arg)
+        elif isinstance(arg, int):
+            total += arg
+    return total
 
-
-summa = 0
 
 # 1 + 2 + 3 + len('a') + 4 + len('b') + 5 + 6 + len('cube') + 7 + .... + 35 = 99
 
 data_structure = [
-[1, 2, 3],
-{'a': 4, 'b': 5},
-(6, {'cube': 7, 'drum': 8}),
-"Hello",
-((), [{(2, 'Urban', ('Urban2', 35))}])
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 
 result = calculate_structure_sum(data_structure)
 print(result)
-
-print(summa)
